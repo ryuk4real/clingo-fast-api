f31cc988505a5d56b8a6edb97aa7706e8cf6abb7
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2023-11-28 00:06:36 +0100

    added post method

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-#!/usr/bin/python3
-
-from fastapi import FastAPI
+import json
+from fastapi import FastAPI, Request
+from clingo import Control, SolveHandle, SolveResult
 
 app = FastAPI()
+control = Control(arguments=[])
 
-@app.get("/")
-def hello_world():
-    return {"Hello":"world"}
+def on_model(model):
+    print(model)
 
+@app.post("/run")
+async def run(request: Request):
+    program = await request.body()
+    program = program.decode("utf-8")
+
+    control.add("base", [], program)
+    control.ground([("base", [])])
+    control.solve(on_model=on_model)
+    
