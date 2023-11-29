6ce767c89384ac9797cf782a5f13b8b98168b78d
ryuk4real <84407165+ryuk4real@users.noreply.github.com>
2023-11-29 22:23:31 +0100

    Handled the solving and the convertion in run controller

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import json
 from fastapi import FastAPI, Request
-from clingo import Control, SolveHandle, SolveResult
+from clingo import Control
+from clingo import Control
+from lib.converter import answer_set_to_json
 
 app = FastAPI()
-control = Control(arguments=[])
+control = Control(arguments=["--opt-mode=optN"])
+answer_sets = []
 
 def on_model(model):
-    print(model)
+    global answer_sets
+    answer_sets.append(answer_set_to_json(str(model)))
 
 @app.post("/run")
 async def run(request: Request):
@@ -15,5 +18,10 @@ async def run(request: Request):
 
     control.add("base", [], program)
     control.ground([("base", [])])
-    control.solve(on_model=on_model)
+    with control.solve(on_model=on_model, async_= True) as handle:
+        handle.wait()
+        print("answer sets: ", answer_sets)
+        return answer_sets
+
+
     
